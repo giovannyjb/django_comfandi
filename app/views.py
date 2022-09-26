4fcb848a2891c70a122e7d22bf1cf7c034ac0281
giovannyjb <giiovaanny11@gmail.com>
2022-09-26 18:02:57 -0500

    routes organizated

--- app/views.py
+++ app/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def hello(request):
-    return HttpResponse("Hello world")
+    return HttpResponse("<h1>Hello world</h1>")
